01bbc0ff73225d12c398eefed6ab01e3a0e3733b
azamat cher <azamatcherikchiev0215@gmail.com>
2020-09-08 06:08:17 -0700

    Adding new class Prozalate

---
+++ src/main/java/aza/Prozalate.java
@@ -0,0 +1,20 @@
+package aza;
+
+public class Prozalate {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String toString(){
+        return "aza"+id;
+
+    }
+    public long isnew(){
+        return this.id;
+    }
+}
